1fc68ae5a44261e4fc093df96f2da21cb4aed69c
AntonKurtsev <kucrev@gmail.com>
2020-07-21 14:56:48 +0300

    create: add hotel-grid__terms &__map

--- js/main.js
+++ js/main.js
@@ -20,3 +20,31 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: true,
   },
 })
+
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [7.991452, 98.303244],
+            zoom: 7
+        }),
+
+        // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [7.991452, 98.303244]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                // iconContent: 'islands#blueHotelIcon',
+                // balloonContent: 'Меня можно перемещать',
+            },
+        },);
+
+    // Добавляем  метку на карту.
+    myMap.geoObjects
+        .add(myGeoObject);
+}
