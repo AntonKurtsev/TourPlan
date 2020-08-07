17e31fdb2189f220f1f27d962b3981745b71eb6f
AntonKurtsev <kucrev@gmail.com>
2020-08-07 12:05:29 +0300

    fix: footer elements positioning

--- send.php
+++ send.php
@@ -25,20 +25,7 @@ elseif ($email = $_POST['email']) {
   ";
 
     header('location: thankyouforsubscribe.html');
-}
-// elseif (($name = $_POST['feedback__name']) && ($phone = $_POST['feedback__phone']) && ($email = $_POST['feedback__email']) && ($message = $_POST['feedback__message'])) {
-//     $title = "Отзыв от попользователя Best Tour Plan";
-//     $body = "
-//     <h2>Новый отзыв</h2>
-//     <b>Имя:</b> $name<br>
-//     <b>Телефон:</b> $phone<br>
-//     <b>E-mail:</b> $email<br>
-//     <b>Сообщение:</b><br>$message
-//     ";
-//     // Отображение результата
-//     header('location: thankyouforfeedback.html');
-//     }
-elseif (($name = $_POST['booking__name']) && ($phone = $_POST['booking__phone']) && ($email = $_POST['booking__email']) && ($message = $_POST['booking__message'])) {
+} elseif (($name = $_POST['booking__name']) && ($phone = $_POST['booking__phone']) && ($email = $_POST['booking__email']) && ($message = $_POST         ['booking__message'])) {
     $title = "Бронирование от Best Tour Plan";
     $body = "
     <h2>Новый запрос на бронирование</h2>
@@ -52,39 +39,52 @@ elseif (($name = $_POST['booking__name']) && ($phone = $_POST['booking__phone'])
 }
   
   // Настройки PHPMailer
-  $mailSubscribe = new PHPMailer\PHPMailer\PHPMailer();
+  $mail = new PHPMailer\PHPMailer\PHPMailer();
   try {
-      $mailSubscribe->isSMTP();   
-      $mailSubscribe->CharSet = "UTF-8";
-      $mailSubscribe->SMTPAuth   = true;
+      $mail->isSMTP();   
+      $mail->CharSet = "UTF-8";
+      $mail->SMTPAuth   = true;
       //$mail->SMTPDebug = 2;
-      $mailSubscribe->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+      $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
   
       // Настройки вашей почты
-      $mailSubscribe->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-      $mailSubscribe->Username   = 'besttourplandrk@gmail.com'; // Логин на почте
-      $mailSubscribe->Password   = '9`]q]H?N'; // Пароль на почте
-      $mailSubscribe->SMTPSecure = 'ssl';
-      $mailSubscribe->Port       = 465;
-      $mailSubscribe->setFrom('besttourplandrk@gmail.com', 'Best Tour Plan'); // Адрес самой почты и имя отправителя
+      $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+      $mail->Username   = 'besttourplandrk@gmail.com'; // Логин на почте
+      $mail->Password   = '9`]q]H?N'; // Пароль на почте
+      $mail->SMTPSecure = 'ssl';
+      $mail->Port       = 465;
+      $mail->setFrom('besttourplandrk@gmail.com', 'Best Tour Plan'); // Адрес самой почты и имя отправителя
   
       // Получатель письма
-      $mailSubscribe->addAddress('kucrev@gmail.com');  
+      $mail->addAddress('kucrev@gmail.com');  
   
   
       // Отправка сообщения
-      $mailSubscribe->isHTML(true);
-      $mailSubscribe->Subject = $title;
-      $mailSubscribe->Body = $body;    
+      $mail->isHTML(true);
+      $mail->Subject = $title;
+      $mail->Body = $body;    
   
       // Проверяем отравленность сообщения
-      if ($mailSubscribe->send()) {$result = "success";} 
+      if ($mail->send()) {$result = "success";} 
       else {$result = "error";}
   
       } catch (Exception $e) {
           $result = "error";
-          $status = "Сообщение не было отправлено. Причина ошибки: {$mailSubscribe->ErrorInfo}";
+          $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
       }
+// elseif (($name = $_POST['feedback__name']) && ($phone = $_POST['feedback__phone']) && ($email = $_POST['feedback__email']) && ($message = $_POST['feedback__message'])) {
+//     $title = "Отзыв от попользователя Best Tour Plan";
+//     $body = "
+//     <h2>Новый отзыв</h2>
+//     <b>Имя:</b> $name<br>
+//     <b>Телефон:</b> $phone<br>
+//     <b>E-mail:</b> $email<br>
+//     <b>Сообщение:</b><br>$message
+//     ";
+//     // Отображение результата
+//     header('location: thankyouforfeedback.html');
+//     }
+
   
       
 
